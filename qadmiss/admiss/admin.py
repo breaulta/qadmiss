d849fcfd1f0e850434b8460a17a326dc1fe169f3
Aaron-WSL <aaronbreault@gmail.com>
2022-03-21 16:20:06 -0700

    initial commit

---
+++ qadmiss/admiss/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import Admiss_app
+
+# Register your models here.
+admin.site.register(Admiss_app)
+
---
+++ qadmiss/admiss/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AdmissConfig(AppConfig):
+    name = 'admiss'
