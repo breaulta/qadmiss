012bc9894c710f8e800a0a08be762d138e305d31
Aaron-WSL <aaronbreault@gmail.com>
2022-03-21 17:56:05 -0700

    first working implementation

--- qadmiss/admiss/urls.py
+++ qadmiss/admiss/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'admiss'
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('app/', views.app, name='app'),
+	path('submit_app/', views.submit_app, name='submit_app'),
+	path('app_submitted/', views.app_submitted, name='app_submitted'),
 ]
--- qadmiss/admiss/views.py
+++ qadmiss/admiss/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 
-# Create your views here.
+from .models import Admiss_app
 
 def index(request):
 	template = loader.get_template('admiss/index.html')
@@ -10,6 +11,18 @@ def index(request):
 	return HttpResponse(template.render(context, request))
 
 def app(request):
-	return HttpResponse("Please fill out the following application.")
+	template = loader.get_template('admiss/app.html')
+	context = {}
+	return HttpResponse(template.render(context, request))
+
+def submit_app(request):
+	app = Admiss_app(first_name=request.POST['first_name'], middle_name=request.POST['middle_name'], last_name=request.POST['last_name'])
+	app.save()
+	return HttpResponseRedirect(reverse('admiss:app_submitted', args=()))
+
+def app_submitted(request):
+	template = loader.get_template('admiss/submitted.html')
+	context = {}
+	return HttpResponse(template.render(context, request))
 
 
